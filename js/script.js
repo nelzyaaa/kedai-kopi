54b28b4fc1dc2d8b9826842aa4ee9d08f7930459
nelzyaaa <nelzya05@gmail.com>
2025-05-13 18:23:07 -0700

    membuat web kopi

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu");
+onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilang nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+    navbarnav.classList.remove("active");
+  }
+});
